1babcae5bb0b002440b8b274718a340212950938
Aryan Prajapati <aryanprajapati9170@gmail.com>
2026-07-22 01:51:01 +0530

    LC-combination-sum-ii [Runtime: N/A; Memory: N/A]

--- array/combination-sum-ii.cpp
+++ array/combination-sum-ii.cpp
@@ -1,35 +1,33 @@
 class Solution {
-public:
-    void findcomb(int ind, int target, vector<int>& arr, set<vector<int>>& ans,
-                  vector<int>& ds) {
-        if (ind == arr.size()) {
-            if (target == 0) {
-                ans.insert(ds);
-            }
+private:
+    void findcomb(int ind, int target, vector<int>& arr,
+                  vector<vector<int>>& ans, vector<int>& ds) {
+
+        if (target == 0) {
+            ans.push_back(ds);
             return;
         }
 
-        // pick the element
-        if (arr[ind] <= target) {
-            ds.push_back(arr[ind]);
-            findcomb(ind + 1, target - arr[ind], arr, ans, ds);
+        for (int i = ind; i < arr.size(); i++) {
+            if (i > ind && arr[i] == arr[i - 1])
+                continue;
+
+            if (arr[i] > target)
+                break;
+
+            ds.push_back(arr[i]);
+            findcomb(i + 1, target - arr[i],arr, ans, ds);
             ds.pop_back();
         }
-
-        // not pick the element
-        findcomb(ind + 1, target, arr, ans, ds);
     }
 
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
-
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         vector<int> ds;
 
         findcomb(0, target, candidates, ans, ds);
-        vector<vector<int>> v(ans.begin(), ans.end());
-
-        return v;
+        return ans;
     }
 };
