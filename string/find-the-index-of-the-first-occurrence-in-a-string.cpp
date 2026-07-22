b9ec353cd1ddb728cb66d63143cb480d125c074f
Aryan Prajapati <aryanprajapati9170@gmail.com>
2026-07-23 00:36:30 +0530

    LC-find-the-index-of-the-first-occurrence-in-a-string [Runtime: N/A; Memory: N/A]

---
+++ string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int strStr(string s, string needle) {
+
+        int x = needle.size();
+        for (int i = 0; i < s.length(); i++) {
+            string st = s.substr(i , i + x);
+            if (st.find(needle) != string ::npos) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
