4b6c414b5c2bff56c103bbd9f573e17fed857f89
Aryan Prajapati <aryanprajapati9170@gmail.com>
2026-07-25 01:28:18 +0530

    LC-sliding-window-maximum [Runtime: N/A; Memory: N/A]

--- monotonic-queue/sliding-window-maximum.cpp
+++ monotonic-queue/sliding-window-maximum.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<pair<int, int>> pq;
         vector<int> ans;
-        deque<int> dq;
 
         for (int i = 0; i < nums.size(); i++) {
+            pq.push({nums[i], i});
 
-            if (!dq.empty() && dq.front() <= i - k) {
-                dq.pop_front();
+            while(!pq.empty() && pq.top().second <= i - k){
+                pq.pop();
             }
-
-            while (!dq.empty() && nums[dq.back()] <= nums[i])
-                dq.pop_back();
-
-            dq.push_back(i);
-
-            if (i >= k - 1)
-                ans.push_back(nums[dq.front()]);
+            if (i >= k - 1) {
+                ans.push_back(pq.top().first);
+            }
         }
-
         return ans;
     }
 };
