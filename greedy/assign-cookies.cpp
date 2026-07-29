bd156b7b2623f0be219d4c0a45aec8dc5a6db614
Aryan Prajapati <aryanprajapati9170@gmail.com>
2026-07-30 02:35:59 +0530

    LC-assign-cookies [Runtime: N/A; Memory: N/A]

--- greedy/assign-cookies.cpp
+++ greedy/assign-cookies.cpp
@@ -9,17 +9,16 @@ public:
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
         for (int i = 0; i < s.size(); i++) {
-            int x = 0;
+            int x = -1;
             for (int j = 0; j < g.size(); j++) {
                 if (s[i] >= g[j]) {
                     x = j;
                     ans++;
                     break;
                 }
-
-                 
             }
-            s.erase(s.begin() + x);
+            if (x != -1)
+                g.erase(g.begin() + x);
             if (s.size() == 0) {
                 return ans;
             }
