ffde07e89b72988eee70b10c8c1704697e855d79
Aryan Prajapati <aryanprajapati9170@gmail.com>
2026-07-31 16:04:15 +0530

    LC-lowest-common-ancestor-of-a-binary-tree [Runtime: N/A; Memory: N/A]

--- depth-first-search/lowest-common-ancestor-of-a-binary-tree.cpp
+++ depth-first-search/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,24 +10,20 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root == nullptr) {
+        if (root == NULL) {
             return nullptr;
         }
 
-        if (root == p || root == q) {
+        if (root == p || root == q)
             return root;
-        }
 
         TreeNode* left = lowestCommonAncestor(root->left, p, q);
         TreeNode* right = lowestCommonAncestor(root->right, p, q);
 
-        if (left != nullptr && right != nullptr) {
+        if(left && right){
             return root;
-        } else if (left != nullptr && right == nullptr) {
-            return left;
-        } else if (left == nullptr && right != nullptr) {
-            return right;
         }
-        return nullptr;
+
+        return left ? left : right;
     }
 };
