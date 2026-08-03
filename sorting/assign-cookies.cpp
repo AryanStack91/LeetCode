5b71081a1f3691da9904bc52febbb83c041b124a
Aryan Prajapati <aryanprajapati9170@gmail.com>
2026-08-04 01:40:44 +0530

    LC-assign-cookies [Runtime: N/A; Memory: N/A]

--- sorting/assign-cookies.cpp
+++ sorting/assign-cookies.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        if (s.size() == 0) {
-            return 0;
-        }
-        int ans = 0;
-
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
-        for (int i = 0; i < g.size(); i++) {
-             for (int j = 0; j < s.size(); j++) {
-                if (s[j] >= g[i]) {
-                     ans++;
-                    break;
-                }
-            }
-             
+
+        int i = 0;
+        int j = 0;
+
+        while (i < g.size() && j < s.size()) {
+            if (g[i] <= s[j]) {
+                i++;
+            }  
+            j++;
         }
-        return ans;
+        return i;
     }
 };
