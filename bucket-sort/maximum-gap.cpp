1d7a6b8aac307a69d1e840d7311aa4f80ecd263a
Aryan Prajapati <aryanprajapati9170@gmail.com>
2026-08-22 00:18:35 +0530

    LC-maximum-gap [Runtime: N/A; Memory: N/A]

--- bucket-sort/maximum-gap.cpp
+++ bucket-sort/maximum-gap.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
+    void bucketSort(vector<int>& nums, int n) {
+        vector<float> bucket[n];
+
+        // Put elements into buckets
+        for (int i = 0; i < n; i++) {
+            int index = n * nums[i];
+            bucket[index].push_back(nums[i]);
+        }
+
+        // Sort each bucket
+        for (int i = 0; i < n; i++)
+            sort(bucket[i].begin(), bucket[i].end());
+
+        // Merge buckets
+        int k = 0;
+        for (int i = 0; i < n; i++) {
+            for (float x : bucket[i])
+                nums[k++] = x;
+        }
+    }
     int maximumGap(vector<int>& nums) {
-        if(nums.size() < 2){
+        if (nums.size() < 2) {
             return 0;
         }
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(), nums.end());
         int ans = 0;
-        for(int i = 1;i < nums.size(); i++){
+        for (int i = 1; i < nums.size(); i++) {
             int diff = nums[i] - nums[i - 1];
-            ans = max(ans,diff);
+            ans = max(ans, diff);
         }
         return ans;
-        
     }
 };
